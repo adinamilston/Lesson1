c492be9fca4b3473804b78fe2265773d596ff593
Adina Milston <milston@g.jct.ac.il>
2020-10-17 23:12:45 +0300

    no cahnges I made really

--- solid45/Program.cs
+++ solid45/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/// <summary>
+/// namespace solid 45 - in Nurit's ppt
+/// </summary>
 namespace solid45
 {
     class Program
